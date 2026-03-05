a02e1c317442a0063afa5b9289141a795e8de6b4
Marcelo <marc.m.amorim@gmail.com>
2026-03-05 11:36:20 -0300

    v1

--- main.js
+++ main.js
@@ -20,6 +20,40 @@ function cifrarAtbash(mensagem) {
     return txtCifrado
 }
 
+function cifrarCesar(mensagem, chave) {
+    let txtCifrado = ""
+    let letrasEntrada = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz"
+    for (let i = 0; i < mensagem.length; i++) {
+        let letra = mensagem.charAt(i);
+        let pos = letrasEntrada.lastIndexOf(letra)
+        let posCifrada = 0
+        if (pos != -1) {
+            if (pos + chave < 0) {
+                posCifrada = letrasEntrada.length + pos + chave
+            } else {
+                posCifrada = (pos + chave) % letrasEntrada.length                
+            }
+            letraCifrada = letrasEntrada.charAt(posCifrada)
+        } else {
+            letraCifrada = letra
+        }
+        txtCifrado = txtCifrado + letraCifrada
+    }
+    return txtCifrado
+}
+
+function acaoCifrarCesar() {
+    let txtOriginal = document.getElementById("txtEntrada").value
+    let txtCifrado = cifrarCesar(txtOriginal, 3)
+    document.getElementById("resp1").innerText = txtCifrado
+}
+
+function acaoDecifrarCesar() {
+    let txtOriginal = document.getElementById("txtSaida").value
+    let txtCifrado = cifrarCesar(txtOriginal, -3)
+    document.getElementById("resp2").innerText = txtCifrado
+}
+
 function cifrar() {
     let txtOriginal = document.getElementById("txtEntrada").value
     let txtCifrado = cifrarAtbash(txtOriginal)
