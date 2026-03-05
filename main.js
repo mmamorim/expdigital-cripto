72bcb052ed738924905291e6be25d647fdbf725e
Marcelo <marc.m.amorim@gmail.com>
2026-03-05 09:59:56 -0300

    v1

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 
 console.log("Oi gente tudo bem?")
 
-function fazAlgo() {
+function cifrar() {
+    let elem = document.getElementById("txtEntrada")
+    console.log("elem: ",elem);
+    console.log("valor: ",elem.value);
+    document.getElementById("resp1").innerText = elem.value
+}
 
-    console.log("Passei por aqui...função FazAlgo");
-    
+function decifrar() {
+    let elem = document.getElementById("txtSaida")
+    console.log("elem: ",elem);
+    console.log("valor: ",elem.value);    
+    document.getElementById("resp2").innerText = elem.value
 }
