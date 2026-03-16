18021f2c960f11990a071663406a8c05a09b82e1
Marcelo <marc.m.amorim@gmail.com>
2026-03-16 18:43:43 -0300

    v1

--- main.js
+++ main.js
@@ -1,30 +1,7 @@
-import cifrarAtbash from "./cifrarAtBash.js";
-import cifrarCesar from "./cifrarCesar.js";
 
 document.getElementById("btnCifrar").onclick = () => {
     let txtOriginal = document.getElementById("txtEntrada").value
     let txtCifrado = cifrarAtbash(txtOriginal)
     document.getElementById("resp1").innerText = txtCifrado
 }
-
-document.getElementById("btnCifrarCesar").onclick = () => {
-    let txtOriginal = document.getElementById("txtEntrada").value
-    let txtCifrado = cifrarCesar(txtOriginal, 3)
-    document.getElementById("resp1").innerText = txtCifrado
-}
-
-document.getElementById("btnDecifrar").onclick = () => {
-    let txtOriginal = document.getElementById("txtSaida").value
-    let txtCifrado = cifrarAtbash(txtOriginal)
-    document.getElementById("resp2").innerText = txtCifrado
-}
-
-document.getElementById("btnDecifrarCesar").onclick = () => {
-    let txtOriginal = document.getElementById("txtSaida").value
-    let txtCifrado = cifrarCesar(txtOriginal, -3)
-    document.getElementById("resp2").innerText = txtCifrado
-}
-
-
-
 
