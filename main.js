d87066d7e541200f87b7c34912d8e942932bbe89
Marcelo <marc.m.amorim@gmail.com>
2026-03-16 19:03:34 -0300

    v1

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 
-document.getElementById("btnCifrar").onclick = () => {
-    let txtOriginal = document.getElementById("txtEntrada").value
-    let txtCifrado = cifrarAtbash(txtOriginal)
-    document.getElementById("resp1").innerText = txtCifrado
-}
 
